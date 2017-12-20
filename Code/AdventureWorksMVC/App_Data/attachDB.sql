b7a42f20a6d744c0f6a38f3ece8d5f84e92f0a4c
Richard Hundhausen <devopsvsts@outlook.com>
2017-12-20 12:21:19 -0700

    Added DB attach and detach scripts

---
+++ Code/AdventureWorksMVC/App_Data/attachDB.sql
@@ -0,0 +1,9 @@
+﻿USE Master
+GO
+
+IF DB_ID('AdventureWorks') IS NULL
+  CREATE DATABASE [AdventureWorks] ON 
+  ( FILENAME = N'C:\inetpub\wwwroot\AdventureWorksMVC_deploy\App_Data\AdventureWorks_Data.mdf' ),
+  ( FILENAME = N'C:\inetpub\wwwroot\AdventureWorksMVC_deploy\App_Data\AdventureWorks_Data_log.ldf' )
+  FOR ATTACH
+GO
